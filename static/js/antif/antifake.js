7fe58e660d79b85d4d5d941ef4bd29d300eb18ea
Fantasy <xcclml@163.com>
2018-02-26 19:20:33 +0800

    质保列表循环

--- static/js/antif/antifake.js
+++ static/js/antif/antifake.js
@@ -1,10 +1,19 @@
-$(document).ready(function(){
-	alert('abc');
-	$('#scanIcon').click(function(){
+$(document).ready(function() {
+	$('#scanIcon').click(function() {
+		wx.scanQRCode({
+			needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+			scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+			success: function(res) {
+				var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+			}
+		});
 		alert('a');
 	});
 });
 
+function searchImg(){
+	
+}
 //$('#scanIcon').click(function(){
 //   alert('你点击了图片');
 //})
--- static/js/history/history.js
+++ static/js/history/history.js
@@ -1,22 +1,37 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 	$.ajax({
-		type:'get',
-		url:'http://wx.hongyancloud.com/wxDev/file/getBillAndDetails',
-		data : {
-			openId : 'oZIooxJ_MT0M1ApB_4caa_gvXgWc',//$.getCookie('open_id')
+		type: 'get',
+		url: 'http://wx.hongyancloud.com/wxDev/file/getBillAndDetails',
+		data: {
+			openId: 'oZIooxJ_MT0M1ApB_4caa_gvXgWc', //$.getCookie('open_id')
 		},
-		success:function(data){
-			if (data.code == "00000") {
+		success: function(returnDatas) {
+			console.log(returnDatas)
+			
+			if(returnDatas.code == "00000") {
+				
+				var returnData = returnDatas.data;
+				
 				var address = $('#address');
-				$(data.data).each(function(i,o){
-					address.append("<li>"+o.address+"</li>");
+				var uploaderFiles = $('#uploaderFiles');
+				var tmpl = '<li class="weui-uploader__file" style="background-image:url(#url#)"></li>';
+				
+				$(returnData).each(function(i, o) {
+					address.append("<li> 地点：" + o.address + "</li>");
+					
+					var children = o.children;
+					$(children).each(function(j, obj) {
+			
+						uploaderFiles.append($(tmpl.replace('#url#', obj.fileRealPath)));
+						console.log(obj.fileRealPath)
+					});
+
 				});
-			}else{
+			} else {
 				weui.topTips(data.msg);
 			}
 		},
-		error:function(data){
+		error: function(data) {
 			weui.topTips('网络异常,请检查您的网络');
 		}
 	});
