da154f61721d628636bdc0b934c22ec85e2c8b50
gq <guanqinghz@163.com>
2018-02-26 15:18:55 +0800

    修改

--- static/js/antif/antifake.js
+++ static/js/antif/antifake.js
@@ -1,3 +1,14 @@
-$('#scanIcon').click(function(){
-   alert('你点击了图片');
-})
+$(document).ready(function(){
+	alert('abc');
+	$('#scanIcon').click(function(){
+		alert('a');
+	});
+});
+
+//$('#scanIcon').click(function(){
+//   alert('你点击了图片');
+//})
+
+//$('#scanIcon').bind('click',function(){
+//   alert('你点击了图片');
+//})
