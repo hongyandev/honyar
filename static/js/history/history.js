9e42aadeba50382ed79760bbc473a43d02350669
Fantasy <xcclml@163.com>
2018-02-27 00:51:43 +0800

    质保列表图片加载

--- static/js/history/history.js
+++ static/js/history/history.js
@@ -7,24 +7,28 @@ $(document).ready(function() {
 		},
 		success: function(returnDatas) {
 			console.log(returnDatas)
-			
+
 			if(returnDatas.code == "00000") {
-				
+
 				var returnData = returnDatas.data;
-				
-				var address = $('#address');
+
+				var details = $('#details');
 				var uploaderFiles = $('#uploaderFiles');
 				var tmpl = '<li class="weui-uploader__file" style="background-image:url(#url#)"></li>';
-				
+
 				$(returnData).each(function(i, o) {
-					address.append("<li> 地点：" + o.address + "</li>");
-					
+					details.append('<div class="deta_h">' + '<h1>' + o.id + '</>' + '</div>');
+					details.append('<div class="deta_ul">' + '<ul id=' + o.id + '>' + '</div>');
+
 					var children = o.children;
+					var uploaderFiles = $('#' + o.id);
+
 					$(children).each(function(j, obj) {
-			
+
 						uploaderFiles.append($(tmpl.replace('#url#', obj.fileRealPath)));
 						console.log(obj.fileRealPath)
 					});
+					details.append('<div style="clear:both;"></div> ');
 
 				});
 			} else {
