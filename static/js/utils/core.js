b267034a516479bc9178dc12a17523f19854f09b
gq <guanqinghz@163.com>
2018-03-13 18:38:08 +0800

    修改 跳转

--- static/js/utils/core.js
+++ static/js/utils/core.js
@@ -6,7 +6,7 @@ var wx = new Object();
 wx.getOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getOpenId';
 wx.getTzOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getTzOpenId';
 wx.removeCookieOpenId = 'http://wx.hongyancloud.com/wxDev/removeCookieOpenId';
-wx.desUrl = 'https://wwww.baidu.com';
+wx.desUrl = 'http://h5.charge.zone.hongyancloud.com/WeiXin/OAuth/Callback?code=';
 wx.appId = 'wx203cc78ceb539356';
 (function($){
 	//获取指定名称的cookie的值
--- static/js/utils/tz.js
+++ static/js/utils/tz.js
@@ -1,12 +1,14 @@
 $(function(){
-	var openId = $.getCookie('tzOpenId');
+	var code = $.getCookie('grant_code');
+	var state = $.getCookie('code_state');
 	var getTzOpenIdUrl = wx.getTzOpenIdUrl;
 	var toUrl = window.location.href;
-	if ($.isNull(openId)) {
+	if ($.isNull(state)) {
 		getTzOpenIdUrl += '?toUrl='+encodeURIComponent(toUrl);
 		var url =   'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+wx.appId+'&redirect_uri=' + encodeURIComponent(getTzOpenIdUrl) +' &response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 		window.location.href = url;
 	}else{
-		window.location.href = wx.desUrl;
+		var url = wx.desUrl + code + "&state = " + state;
+		window.location.href = wx.desUrl+code+"&state="+state;
 	}
 });
