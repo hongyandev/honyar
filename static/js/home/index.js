5d3cfe70c80b85182931e9e0835ba0a3b8f2cf05
Fantasy <xcclml@163.com>
2018-03-13 10:28:17 +0800

    index修改恢复

--- static/js/home/index.js
+++ static/js/home/index.js
@@ -14,12 +14,12 @@ jQuery(document).ready(function($) {
 		smartSpeed: 450
 	});
 	
-	/*if (!$.isNull(decodeURIComponent($.getCookie('head_url')))) {
+	if (!$.isNull(decodeURIComponent($.getCookie('head_url')))) {
 		$('#headImg').attr('src',decodeURIComponent($.getCookie('head_url')));
 	}
 	if (!$.isNull(decodeURIComponent($.getCookie('nick_name')))) {
 		$('#nickName').html(decodeURIComponent($.getCookie('nick_name')));
-	}*/
+	}
 });
 
 document.addEventListener('touchmove', function(e) {
