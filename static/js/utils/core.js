db4157db4e2559d61086c772cf186b8665c59c38
gq <guanqinghz@163.com>
2018-03-29 16:52:20 +0800

    修改

--- static/js/utils/core.js
+++ static/js/utils/core.js
@@ -2,12 +2,12 @@
  *	create by GQ
  *	2018.01.24 13:23
  */
-var wx = new Object();
-wx.getOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getOpenId';
-wx.getTzOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getTzOpenId';
-wx.removeCookieOpenId = 'http://wx.hongyancloud.com/wxDev/removeCookieOpenId';
-wx.desUrl = 'http://h5.charge.zone.hongyancloud.com/WeiXin/OAuth/Callback?code=';
-wx.appId = 'wx203cc78ceb539356';
+var req = new Object();
+req.getOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getOpenId';
+req.getTzOpenIdUrl = 'http://wx.hongyancloud.com/wxDev/getTzOpenId';
+req.removeCookieOpenId = 'http://wx.hongyancloud.com/wxDev/removeCookieOpenId';
+req.desUrl = 'http://h5.charge.zone.hongyancloud.com/WeiXin/OAuth/Callback?code=';
+req.appId = 'wx203cc78ceb539356';
 (function($){
 	//获取指定名称的cookie的值
 	$.getCookie = function(objname){
--- static/js/utils/getWxOpenId.js
+++ static/js/utils/getWxOpenId.js
@@ -4,13 +4,13 @@
  */
 $(function(){
 	var openId = $.getCookie('open_id');
-	var getOpenIdUrl = wx.getOpenIdUrl;
+	var getOpenIdUrl = req.getOpenIdUrl;
 	var toUrl = window.location.href;
 	if ($.isNull(openId)) {
 		getOpenIdUrl += '?toUrl='+encodeURIComponent(toUrl);
-		var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+wx.appId+'&redirect_uri=' + encodeURIComponent(getOpenIdUrl) + '&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect';
+		var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+req.appId+'&redirect_uri=' + encodeURIComponent(getOpenIdUrl) + '&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect';
 		window.location.href = url;
 	}//else{
-		//window.location.href = wx.removeCookieOpenId;
+		//window.location.href = req.removeCookieOpenId;
 	//}
 });
