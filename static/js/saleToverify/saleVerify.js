19ac83d497ff8d3d9975c2766317ea32111b155d
Chai <chai@fy.com>
2018-04-02 14:58:25 +0800

    核销页面已完成（测试）

--- static/js/saleToverify/saleVerify.js
+++ static/js/saleToverify/saleVerify.js
@@ -29,6 +29,19 @@ $(function () {
         var customer = $('#customer').val();
         var totalMoney =$('#totalMoney').val();
         var discountAmount=$('#discountAmount').val();
+        var formdata = new FormData();
+        for(var index in uploadFiles) {
+            formdata.append("files", uploadFiles[index][0]);
+        }
+        formdata.append('openId', openID);
+        formdata.append('uid','$(".sCodeInput").val()');
+        formdata.append('khdm','$("#khdm").val()');
+        formdata.append('khmc','$(".khmc").html()');
+        formdata.append('name','$("#name").val()');
+        formdata.append('telephone','$("#telephone").val()');
+        formdata.append('customer','$("#customer").val()');
+        formdata.append('totalMoney','$("#totalMoney").val()');
+        formdata.append('discountAmount','$("#discountAmount").val()');
         if(customer == "") {
             weui.topTips('请输入顾客姓名',3000);
             return;
@@ -41,12 +54,12 @@ $(function () {
             weui.topTips('请输入优惠金额',3000);
             return;
         }
-        var formData={};
-        var arr=[];
-        for(var i = 0;i<uploadFiles.length;i++){
-            arr.push(uploadFiles[i][0]);
-        }
-        formData.files=arr;
+        /* var formData={};
+         var arr=[];
+         for(var i = 0;i<uploadFiles.length;i++){
+             arr.push(uploadFiles[i][0]);
+         }
+         formData.files=arr;
         formData.openId=openID;
         formData.uid=$('.sCodeInput').val();
         formData.khdm=$('#khdm').val();
@@ -55,23 +68,20 @@ $(function () {
         formData.name=$('#name').val();
         formData.customer=$('#customer').val();
         formData.totalMoney=$('#totalMoney').val();
-        formData.discountAmount=$('#discountAmount').val();
-        console.log(formData);
+        formData.discountAmount=$('#discountAmount').val();*/
+        //console.log(formData);
         //return;
+        console.info(formdata);
         $.ajax({
-            type: "POST",
+            type: "post",
             url: "http://wx.hongyancloud.com/wxDev/verificate/saveVerificateAndDetails",
-            data: formData,
-            dataType:"json",
+            data: formdata,
             timeout: 5000,
             //必须false才会避开jQuery对 formdata 的默认处理
             // XMLHttpRequest会对 formdata 进行正确的处理
             processData: false,
             //必须false才会自动加上正确的Content-Type
             contentType: false,
-            xhrFields: {
-                withCredentials: false
-            },
             success: function(data){
                 loading.hide();
                 if(data.code == "00000") {
