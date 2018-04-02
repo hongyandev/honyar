660dbaab6e95aad0941b7bead72e25b63790c331
Chai <chai@fy.com>
2018-04-02 09:15:24 +0800

    核销页面已完成（测试）

--- static/js/saleToverify/saleVerify.js
+++ static/js/saleToverify/saleVerify.js
@@ -1,22 +1,22 @@
 $(function () {
     var openID=$.getCookie('open_id');
-    //var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
+    // var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
     $.ajax({
         type: "get",
         url: "http://wx.hongyancloud.com/wxDev/verificate/authority?openId="+openID,
         dataType:"json",
         success: function(data) {
-        if(data.code != "00000") {
-            window.location.href="error.html?error="+data.msg;
-        }else {
-            if(data.code == "00000") {
-                jQuery("#khdm").val(data.data.khdm);
-                jQuery(".khmc").html(data.data.khmc);
-                jQuery("#telephone").val(data.data.telephone);
-                jQuery("#name").val(data.data.name);
+            if(data.code != "00000") {
+                window.location.href="error.html?error="+data.msg;
+            }else {
+                if(data.code == "00000") {
+                    jQuery("#khdm").val(data.data.khdm);
+                    jQuery(".khmc").html(data.data.khmc);
+                    jQuery("#telephone").val(data.data.telephone);
+                    jQuery("#name").val(data.data.name);
+                }
             }
-        }
-    }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+        }, error: function(XMLHttpRequest, textStatus, errorThrown) {
 
         }
     });
@@ -25,10 +25,28 @@ $(function () {
         var loading = weui.loading('正在提交...', {
             className: 'custom-classname'
         });
-        var openID=$.getCookie('open_id');
-        var customer = $('#customer').val();
-        var totalMoney =$('#totalMoney').val();
-        var discountAmount=$('#discountAmount').val();
+        // var openID=$.getCookie('open_id');
+        // var customer = $('#customer').val();
+        // var totalMoney =$('#totalMoney').val();
+        // var discountAmount=$('#discountAmount').val();
+
+        // var formdata = new FormData();
+        // for(var index in uploadFiles) {
+        //     formdata.append("files", uploadFiles[index]);
+        //     console.info(uploadFiles[index]);
+        // }
+        // formdata.append('openId', openID);
+        // formdata.append('uid',$('.sCodeInput').val());
+        // formdata.append('khdm', $('#khdm').val());
+        // formdata.append('khmc', $('.khmc').html());
+        // formdata.append('telephone',$('#telephone').val());
+        // formdata.append('name',$('#name').val());
+        // formdata.append('customer',$('#customer').val());
+        // formdata.append('totalMoney',$('#totalMoney').val());
+        // formdata.append('discountAmount',$('#discountAmount').val());
+
+
+        /*表单验证*/
         if(customer == "") {
             weui.topTips('请输入顾客姓名',3000);
             return;
@@ -41,25 +59,27 @@ $(function () {
             weui.topTips('请输入优惠金额',3000);
             return;
         }
-        var formdata = new FormData();
-        for(var index in uploadFiles) {
-            formdata.append("files", uploadFiles[index][0]);
-            //console.info(uploadFiles)
+        var formData={};
+        var arr=[];
+        for(var i = 0;i<uploadFiles.length;i++){
+            arr.push(uploadFiles[i][0]);
         }
-        formdata.append('openId', openID);
-        formdata.append('uid',$('.sCodeInput').val());
-        formdata.append('khdm', $('#khdm').val());
-        formdata.append('khmc', $('.khmc').html());
-        formdata.append('telephone',$('#telephone').val());
-        formdata.append('name',$('#name').val());
-        formdata.append('customer',$('#customer').val());
-        formdata.append('totalMoney',$('#totalMoney').val());
-        formdata.append('discountAmount',$('#discountAmount').val());
-
+        formData.files=arr;
+        formData.openId=openID;
+        formData.uid=$('.sCodeInput').val();
+        formData.khdm=$('#khdm').val();
+        formData.khmc=$('.khmc').html();
+        formData.telephone=$('#telephone').val();
+        formData.name=$('.name').val();
+        formData.customer=$('.customer').val();
+        formData.totalMoney=$('.totalMoney').val();
+        formData.discountAmount=$('.discountAmount').val();
+        console.log(formData);
+        //return;
         $.ajax({
             type: "post",
             url: "http://wx.hongyancloud.com/wxDev/verificate/saveVerificateAndDetails",
-            data: formdata,
+            data: formData,
             timeout: 5000,
             //必须false才会避开jQuery对 formdata 的默认处理
             // XMLHttpRequest会对 formdata 进行正确的处理
