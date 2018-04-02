1ff9f4a474ecc52ade186a176842fcc6942bde90
Chai <chai@fy.com>
2018-04-02 15:32:55 +0800

    核销页面已完成（测试）

--- static/js/saleToverify/saleVerify.js
+++ static/js/saleToverify/saleVerify.js
@@ -33,6 +33,7 @@ $(function () {
         formdata.append('openId', openID);
         formdata.append('uid',$(".sCodeInput").val());
         formdata.append('khdm',$("#khdm").val());
+        formdata.append('khmc',$(".khmc").html());
         formdata.append('name',$("#name").val());
         formdata.append('telephone',$("#telephone").val());
         formdata.append('customer',$("#customer").val());
