732eab763788ba285b0f76f65bd397f9bb421aaf
Chai <chai@fy.com>
2018-04-08 11:30:39 +0800

    核销页面

--- static/js/saleToverify/saleVerifyList.js
+++ static/js/saleToverify/saleVerifyList.js
@@ -2,10 +2,87 @@ $(function () {
     var openID=$.getCookie('open_id');
     //var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
     var loading = false;
+    var page=1;
     record();
 
-    //删除
-    $(document).on("click", ".delete-swipeout", function() {
+//向下拉
+$(document.body).pullToRefresh().on("pull-to-refresh", function() {
+        setTimeout(function() {
+            page = 1;
+            $(".infoList").html("");
+            record();
+            $(".weui-loadmore").html("<i class=\"weui-loading\"></i>\n" +
+                "    <span class=\"weui-loadmore__tips\">上拉加载更多</span>");
+            $(document.body).pullToRefreshDone();
+        }, 1000);
+    });
+
+//向上拉
+$(document.body).infinite().on("infinite", function() {
+    page++;
+    setTimeout(function(){
+        $.ajax({
+            type: "get",
+            url: "http://wx.hongyancloud.com/wxDev/verificate/getVerificateFenye?page="+page,
+            dataType: "json",
+            data: {
+                "openId":openID,
+                "pageNum":page++,
+                "pageSize":"6"
+            },success: function(res) {
+                if(res.code == "00000") {
+                    if(res.data.pages < page){
+                        $(".weui-loadmore").html(" <span class=\"weui-loadmore__tips\">我已经到底了...</span>");
+                        return;
+                    }
+                    var str="";
+                    $.each(res.data.list,function (index,val){
+                            str+= "<li class=\"weui-cell weui-cell_swiped\"  value='"+val.id+"'>"+
+                                " <div class=\"weui-cell__bd\" style=\"transform: translate3d(0px, 0px, 0px);\">\n" +
+                                "                <div class=\"weui-cell__bd\">\n";
+                            if(val.status==1){
+                                str+= "<p class='binfo clear'><em class='fri red'>"+val.desc+"</em></p>";
+                            }else{
+                                str+= "<p class='binfo clear'><em class='fri gray'>"+val.desc+"</em></p>";
+                            }
+                            str+="                    <p class=\"cInfo\">\n" +
+                                "                        <span class=\"font14\">"+val.customer+"</span>\n" +
+                                "                        <span class=\"font12\">共计:<b class=\"font14\">￥"+val.totalMoney+"</b></span>\n" +
+                                "                        <span class=\"font12\">优惠:<b class=\"font14\">￥"+val.discountAmount+"</b></span>\n" +
+                                "                    </p>\n" +
+                                "                    <div class=\"binfo clear\">\n" +
+                                "                        <span class=\"fle\">"+val.name+"&nbsp;&nbsp;</span>\n" +
+                                "                        <span class=\"fle\">"+val.telephone+"</span>\n" +
+                                "                        <time class=\"fri\">"+val.verificateDate+"</time>\n" +
+                                "                    </div>\n" +
+                                "                </div>\n" +
+                                "         </div>\n" ;
+                            if(val.status==1){
+                                str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
+                                    "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout\" href=\"javascript:\">删除</button>\n" +
+                                    "</div>";
+                            }else{
+                                str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
+                                    "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout btnGray\" disabled='disabled' href=\"javascript:\">删除</button>\n" +
+                                    "</div>";
+                            }
+                            str+= "</li>"
+                        });
+                    $('.infoList').append(str);
+
+                }
+                $('.weui-cell_swiped').swipeout();
+            }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+
+            }
+
+        });
+        loading = false;
+    }, 1000);
+});
+
+//删除
+$(document).on("click", ".delete-swipeout", function() {
         var recordid=$(this).parents("li").attr("value");
         $.confirm("", "您确定要删除文件这条记录吗?", function() {
             $.ajax({
@@ -29,153 +106,84 @@ $(function () {
         });
     });
 
-//向下拉
-    $(document.body).pullToRefresh().on("pull-to-refresh", function() {
-        setTimeout(function() {
-            page = 1;
-            $(".infoList").html("");
-            record();
-            $(".weui-loadmore").html("<i class=\"weui-loading\"></i>\n" +
-                "    <span class=\"weui-loadmore__tips\">上拉加载更多</span>");
-            $(document.body).pullToRefreshDone();
-        }, 1000);
-    });
+//record()
+ function record(){
+        $.ajax({
+            type: "get",
+            url: "http://wx.hongyancloud.com/wxDev/verificate/getVerificateFenye?page="+page,
+            dataType:"json",
+            data: {
+                "openId":openID,
+                "pageNum":page,
+                "pageSize":"6"
+            },
+            success: function(res) {
 
+                if(res.code == "00000") {
+                    if(res.data.pages <= page){
+                       // $(".weui-loadmore").html(" <span class=\"weui-loadmore__tips\">已经加载完全部数据</span>")
+                       $(".weui-loadmore").hide();
+                    }
+                    var str="";
+                    $.each(res.data.list,function (index,val){
+                        str+= "<li class=\"weui-cell weui-cell_swiped\"  value='"+val.id+"'>"+
+                            " <div class=\"weui-cell__bd\" style=\"transform: translate3d(0px, 0px, 0px);\">\n" +
+                            "                <div class=\"weui-cell__bd\">\n";
+                        if(val.status==1){
+                            str+= "<p class='binfo clear'><em class='fri red'>"+val.desc+"</em></p>";
+                        }else{
+                            str+= "<p class='binfo clear'><em class='fri gray'>"+val.desc+"</em></p>";
+                        }
+                        str+="                    <p class=\"cInfo\">\n" +
+                            "                        <span class=\"font14\">"+val.customer+"</span>\n" +
+                            "                        <span class=\"font12\">共计:<b class=\"font14\">￥"+val.totalMoney+"</b></span>\n" +
+                            "                        <span class=\"font12\">优惠:<b class=\"font14\">￥"+val.discountAmount+"</b></span>\n" +
+                            "                    </p>\n" +
+                            "                    <div class=\"binfo clear\">\n" +
+                            "                        <span class=\"fle\">"+val.name+"&nbsp;&nbsp;</span>\n" +
+                            "                        <span class=\"fle\">"+val.telephone+"</span>\n" +
+                            "                        <time class=\"fri\">"+val.verificateDate+"</time>\n" +
+                            "                    </div>\n" +
+                            "                </div>\n" +
+                            "         </div>\n" ;
+                        if(val.status==1){
+                            str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
+                                "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout\" href=\"javascript:\">删除</button>\n" +
+                                "</div>";
+                        }else{
+                            str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
+                                "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout btnGray\" disabled='disabled' href=\"javascript:\">删除</button>\n" +
+                                "</div>";
+                        }
+
+                        str+= "</li>"
+                    });
+                    $('.infoList').html(str);
+
+
+                }else{
+                    weui.topTips(res.msg);
+                };
+                $('.weui-cell_swiped').swipeout();
+
+
+
+            }, error: function(XMLHttpRequest, textStatus, errorThrown) {
+
+            }
+        });
+    }
 });
-function record(){
-    var openID=$.getCookie('open_id');
-    //var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
-    var page = 1;
-    var loading = false;
-    $.ajax({
-        type: "get",
-        url: "http://wx.hongyancloud.com/wxDev/verificate/getVerificateFenye?page="+page,
-        dataType:"json",
-        data: {
-            "openId":openID,
-            "pageNum":page,
-            "pageSize":"10"
-        },
-        success: function(res) {
-
-            if(res.code == "00000") {
-
-                var str="";
-                $.each(res.data.list,function (index,val){
-                    str+= "<li class=\"weui-cell weui-cell_swiped\"  value='"+val.id+"'>"+
-                        " <div class=\"weui-cell__bd\" style=\"transform: translate3d(0px, 0px, 0px);\">\n" +
-                        "                <div class=\"weui-cell__bd\">\n";
-                    if(val.status==1){
-                        str+= "<p class='binfo clear'><em class='fri red'>"+val.desc+"</em></p>";
-                    }else{
-                        str+= "<p class='binfo clear'><em class='fri gray'>"+val.desc+"</em></p>";
-                    }
-                    str+="                    <p class=\"cInfo\">\n" +
-                        "                        <span class=\"font14\">"+val.customer+"</span>\n" +
-                        "                        <span class=\"font12\">共计:<b class=\"font14\">￥"+val.totalMoney+"</b></span>\n" +
-                        "                        <span class=\"font12\">优惠:<b class=\"font14\">￥"+val.discountAmount+"</b></span>\n" +
-                        "                    </p>\n" +
-                        "                    <div class=\"binfo clear\">\n" +
-                        "                        <span class=\"fle\">"+val.name+"&nbsp;&nbsp;</span>\n" +
-                        "                        <span class=\"fle\">"+val.telephone+"</span>\n" +
-                        "                        <time class=\"fri\">"+val.verificateDate+"</time>\n" +
-                        "                    </div>\n" +
-                        "                </div>\n" +
-                        "         </div>\n" ;
-                    if(val.status==1){
-                       str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
-                             "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout\" href=\"javascript:\">删除</button>\n" +
-                             "</div>";
-                    }else{
-                        str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
-                              "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout btnGray\" disabled='disabled' href=\"javascript:\">删除</button>\n" +
-                             "</div>";
-                    }
-
-                     str+= "</li>"
-                });
-
-                $('.infoList').html(str);
 
 
-            }else{
-                weui.topTips(res.msg);
-            };
-            $('.weui-cell_swiped').swipeout();
 
-                //向上拉
-                $(document.body).infinite().on("infinite", function() {
-                    if(loading) return;
-                    loading = true;
-                    setTimeout(function(){
 
-                        $.ajax({
-                            type: "get",
-                            url: "http://wx.hongyancloud.com/wxDev/verificate/getVerificateFenye?page="+page,
-                            dataType: "json",
-                            data: {
-                                "openId":openID,
-                                "pageNum":page++,
-                                "pageSize":"10"
-                            },success: function(res) {
 
-                                if(res.code == "00000") {
-                                    var str="";
-                                    if (res.data.list.length > 0){
 
-                                        $.each(res.data.list,function (index,val){
-                                            str+= "<li class=\"weui-cell weui-cell_swiped\"  value='"+val.id+"'>"+
-                                                " <div class=\"weui-cell__bd\" style=\"transform: translate3d(0px, 0px, 0px);\">\n" +
-                                                "                <div class=\"weui-cell__bd\">\n";
-                                            if(val.status==1){
-                                                str+= "<p class='binfo clear'><em class='fri red'>"+val.desc+"</em></p>";
-                                            }else{
-                                                str+= "<p class='binfo clear'><em class='fri gray'>"+val.desc+"</em></p>";
-                                            }
-                                            str+="                    <p class=\"cInfo\">\n" +
-                                                "                        <span class=\"font14\">"+val.customer+"</span>\n" +
-                                                "                        <span class=\"font12\">共计:<b class=\"font14\">￥"+val.totalMoney+"</b></span>\n" +
-                                                "                        <span class=\"font12\">优惠:<b class=\"font14\">￥"+val.discountAmount+"</b></span>\n" +
-                                                "                    </p>\n" +
-                                                "                    <div class=\"binfo clear\">\n" +
-                                                "                        <span class=\"fle\">"+val.name+"&nbsp;&nbsp;</span>\n" +
-                                                "                        <span class=\"fle\">"+val.telephone+"</span>\n" +
-                                                "                        <time class=\"fri\">"+val.verificateDate+"</time>\n" +
-                                                "                    </div>\n" +
-                                                "                </div>\n" +
-                                                "         </div>\n" ;
-                                            if(val.status==1){
-                                                str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
-                                                    "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout\" href=\"javascript:\">删除</button>\n" +
-                                                    "</div>";
-                                            }else{
-                                                str+= "<div class=\"weui-cell__ft\" status='"+val.status+"'>\n" +
-                                                    "<button class=\"weui-swiped-btn weui-swiped-btn_warn delete-swipeout btnGray\" disabled='disabled' href=\"javascript:\">删除</button>\n" +
-                                                    "</div>";
-                                            }
-                                            str+= "</li>"
-                                        });
-                                        $('.infoList').append(str);
-                                    }else{
-                                        $(".weui-loadmore").html(" <span class=\"weui-loadmore__tips\">我已经到底了...</span>");
-                                        loading = true;
-                                    }
 
-                                }
-                                $('.weui-cell_swiped').swipeout();
-                            }, error: function(XMLHttpRequest, textStatus, errorThrown) {
 
-                            }
 
-                        });
 
-                        loading = false;
-                    }, 1000);
-                });
 
-        }, error: function(XMLHttpRequest, textStatus, errorThrown) {
 
-        }
-    });
-}
 
