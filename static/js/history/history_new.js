2d289f13795f6ab2bcd831b73822c9450ab42de9
Chai <chai@fy.com>
2018-04-17 09:29:04 +0800

    测试配置

--- static/js/history/history_new.js
+++ static/js/history/history_new.js
@@ -1,6 +1,6 @@
 function Bt_submit() {
 
-//	var openID = 'oZlooxHvjmiadlhZXf_40nVrHgd4';
+//var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
 	var openID = $.getCookie('open_id'); //'oZlooxHvjmiadlhZXf_40nVrHgd4';// $.getCookie('open_id');
 	var itmType = $('#selType').val();
 	var describe = $('#inputName').val();
