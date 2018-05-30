c4464989e3a4d2dcd224af5fec9b36757aea414c
Chai <chai@fy.com>
2018-05-30 10:51:49 +0800

    测试配置

--- static/js/appiontment/myApppiont_v.js
+++ static/js/appiontment/myApppiont_v.js
@@ -5,35 +5,87 @@ $(function() {
 			openId: "",
 			pageNum: "",
 			pageSize: "",
+            telephone:"",
 			workersItem: []
 		},
-		methods: {
-			postuser: function(item){
-                $.confirm("", "您确定要删除这条预约吗?", function() {
-                    $.ajax({
-                        type:"post",
-                        url:genAPI('/wxDev/reserve/deleteReserve'),
-                        dataType:"json",
-                        data: {
-                            "openId":this.openId,
-                            "id":item.id
-                        }, success: function(res) {
-                            if(res.code==00000){
-                                //console.info(this.id);
-                                $.toast("预约已删除!", 2000,function() {
-                                    window.location.href=window.location.href;
-                                });
+        watch: {
+            telephone:function(val, oldVal){
+                //console.info(val+","+oldVal)
+                if(val==""){
+                    this.doSearch();
+                }
+            }
+        },
+        methods: {
+            postuser: function(item){
+                if(this.telephone == ""){
+                    $.confirm("", "您确定要删除这条预约吗?", function() {
+                        $.ajax({
+                            type:"post",
+                            url:genAPI('/wxDev/reserve/deleteReserve'),
+                            dataType:"json",
+                            data: {
+                                "uid":this.uid,
+                                "id":item.id
+                            }, success: function(res) {
+                                if(res.code==00000){
+                                    $.toast("预约已删除!", 2000,function() {
+                                        window.location.href=window.location.href;
+                                    });
+                                }
                             }
-                        }
+                        });
+                    }, function() {
+                        $('.weui-cell_swiped').swipeout('close');
+                        //取消操作
                     });
-                }, function() {
-                    $('.weui-cell_swiped').swipeout('close');
-                    //取消操作
+                }else{
+                    $.confirm("", "很抱歉，您删除不了这条预约", function() {
+                        $('.weui-cell_swiped').swipeout('close');
+                    })
+                }
+
+            },
+            cancelSearch:function () {
+                $.ajax({
+                    url:genAPI('wxDev/reserve/getReserveFenye'),
+                    //url: "http://wxdev.hongyancloud.com/wxDev/reserve/getReserveFenye",
+                    async: false,
+                    type: 'GET',
+                    data: {
+                        openId: this.openId,
+                        pageNum: this.pageNum,
+                        pageSize: this.pageSize
+                    },
+                    dataType: 'json',
+                    success: function(data) {
+                        var datas = data.data.list;
+                        //console.log(datas);
+                        this.workersItem = datas;
+                        $('.weui-cell_swiped').swipeout();
+                    },
+                    error: function(data) {
+                        console.log(data)
+                    },
+                    complete: function() {
+                       // console.info(this.telephone);
+                        this.telephone="";
+                       // console.log('complete')
+//						$.hideLoading();
+                    },
+                    context: this
                 });
             },
 			doSearch: function(e) {
-				console.log(e)
-				var telephone = e ? e.target.value : '';
+				//console.log(e);
+				//var telephone = e ? e.target.value : '';
+                var phone = this.telephone.toUpperCase();
+                //console.log(phone);
+
+                if(isPhoneNo(phone) == false) {
+                    $.toptip('请输入正确的手机号码',3000);
+                    return;
+                };
 //				$.showLoading();
 				$.ajax({
                     url:genAPI('wxDev/reserve/getReserveFenye'),
@@ -42,14 +94,14 @@ $(function() {
 					type: 'GET',
 					data: {
 						openId: this.openId,
-						telephone: telephone,
+						telephone: phone,
 						pageNum: this.pageNum,
 						pageSize: this.pageSize
 					},
 					dataType: 'json',
 					success: function(data) {
 						var datas = data.data.list;
-						console.log(datas);
+						//console.log(datas);
 						this.workersItem = datas;
                         $('.weui-cell_swiped').swipeout();
 					},
@@ -70,6 +122,7 @@ $(function() {
 		mounted: function() {
 			console.log("init...")
 			//this.openId = "oZIooxJ_MT0M1ApB_4caa_gvXgWc";
+			//this.openId ="oZIooxC8FeMLlPp3n6fwuiT44trk";
 			this.openId = $.getCookie('open_id');
 			this.pageNum = 1;
 			this.pageSize = 10000;
