0ed2795235124b7829b7dd6828b247b2889cf5e3
Chai <chai@fy.com>
2019-06-19 13:51:56 +0800

    质保记录

--- static/js/history/history.js
+++ static/js/history/history.js
@@ -3,33 +3,83 @@ $(function() {
     //var openID = 'oZIooxJ_MT0M1ApB_4caa_gvXgWc';
     var openID=$.getCookie('open_id');
     loadList('getBillAndDetails', openID);
-
+    //检索
     var $searchBar = $('#searchBar'),
         $searchResult = $('#searchResult'),
         $searchText = $('#searchText'),
         $searchInput = $('#searchInput'),
         $searchClear = $('#searchClear'),
         $searchCancel = $('#searchCancel');
+    $searchText.on('click', function() {
+        $searchBar.addClass('weui-search-bar_focusing');
+        $searchInput.focus();
+    });
 
     function hideSearchResult() {
         $searchResult.hide();
         $searchInput.val('');
     }
-
+    //取消按钮
     function cancelSearch() {
         hideSearchResult();
         $searchBar.removeClass('weui-search-bar_focusing');
         $searchText.show();
-        loadList('getBillAndDetails', openID)
+        loadList('getBillAndDetails', openID);
     }
-
-    $searchText.on('click', function() {
-        $searchBar.addClass('weui-search-bar_focusing');
-        $searchInput.focus();
-    });
     $searchInput.on('blur', function() {
         if(!this.value.length) cancelSearch();
     });
+    $("#souBtn").on('click', function() {
+        var telephone = $('#searchInput').val();
+        if(telephone == ""){
+            weui.topTips("请输入手机号码");
+            return;
+        };
+        if(isPhoneNo(telephone) == true) {
+            console.info(telephone);
+            loadList('searchBill', openID, telephone);
+            //$searchResult.show();
+           // return;
+
+        } else {
+            weui.topTips("请填写正确的手机号码",3000);
+        }
+    });
+    $searchClear.on('click', function() {
+        hideSearchResult();
+        cancelSearch();
+        $searchInput.focus();
+    });
+    $searchCancel.on('click', function() {
+        cancelSearch();
+        $searchInput.blur();
+    });
+   /* var $searchBar = $('#searchBar'),
+        $searchResult = $('#searchResult'),
+        $searchText = $('#searchText'),
+        $searchInput = $('#searchInput'),
+        $searchClear = $('#searchClear'),
+        $searchCancel = $('#searchCancel');
+
+     function hideSearchResult() {
+         $searchResult.hide();
+         $searchInput.val('');
+     }
+
+     function cancelSearch() {
+         hideSearchResult();
+         $searchBar.removeClass('weui-search-bar_focusing');
+         $searchText.show();
+         loadList('getBillAndDetails', openID)
+     }
+
+     $searchText.on('click', function() {
+         $searchBar.addClass('weui-search-bar_focusing');
+         $searchInput.focus();
+     });
+     $searchInput.on('blur', function() {
+         if(!this.value.length) cancelSearch();
+     });
     $("#souBtn").on('click', function() {
         var value = $searchInput.val();
         if(value.length==""){
@@ -50,7 +100,7 @@ $(function() {
     $searchCancel.on('click', function() {
         cancelSearch();
         $searchInput.blur();
-    });
+    });*/
 });
 
 //刷新加载页面
@@ -74,24 +124,31 @@ function loadList(action, openid, keyword) {
                 var details = $('#details');
                 details.empty();
                 var str='';
-                $.each(returnData,function (i,o){
-                    var clickBT = '<a href="history_pic.html?mainid=' + o.id + '" class="addPic"></a>';
-                    var deleteBT = '<a href="javascript:void(0)" class="deletePic"></a>';
-                    str+="<div class='detaId'>";
-                    str+="<div class='deta_h'><h1>"+ o.address +"</h1></div><div class='deta_ul' openid="+o.openId+">";
-                    str+="<ul class='clear detaPic' id="+o.id+">";
-                    $.each(o.children,function (j,obj) {
-                        //str+="<li class='weui-uploader__file' imgid="+obj.id+" realPath='background-image:url("+obj.fileRealPath+")' style='background-image:url("+obj.fileRealPath+"?x-oss-process=image/resize,m_fill,h_100,w_100)'></li>"
-                        str+="<li class='weui-uploader__file' imgid="+obj.id+" realPath='"+obj.fileRealPath+"'>" +
-							"<img src='"+obj.fileRealPath+"?x-oss-process=image/resize,m_fill,h_100,w_100'/>"+
-							"</li>"
+                if(returnData.length > 0){
+                    $.each(returnData,function (i,o){
+                        var clickBT = '<a href="history_pic.html?mainid=' + o.id + '" class="addPic"></a>';
+                        var deleteBT = '<a href="javascript:void(0)" class="deletePic"></a>';
+                        str+="<div class='detaId'>";
+                        str+="<div class='deta_h'><h1>"+ o.address +"</h1></div><div class='deta_ul' openid="+o.openId+">";
+                        str+="<ul class='clear detaPic' id="+o.id+">";
+                        $.each(o.children,function (j,obj) {
+                            //str+="<li class='weui-uploader__file' imgid="+obj.id+" realPath='background-image:url("+obj.fileRealPath+")' style='background-image:url("+obj.fileRealPath+"?x-oss-process=image/resize,m_fill,h_100,w_100)'></li>"
+                            str+="<li class='weui-uploader__file' imgid="+obj.id+" realPath='"+obj.fileRealPath+"'>" +
+                                "<img src='"+obj.fileRealPath+"?x-oss-process=image/resize,m_fill,h_100,w_100'/>"+
+                                "</li>"
 
+                        });
+                        str+="</ul>";
+                        str+='<ol value="'+o.id+'" class="operatBtn clear"><li>'+clickBT+'</li><li class="delpic">'+deleteBT+'</li></ol></div>';
+                        str+='</div>';
                     });
-                    str+="</ul>";
-                    str+='<ol value="'+o.id+'" class="operatBtn clear"><li>'+clickBT+'</li><li class="delpic">'+deleteBT+'</li></ol></div>';
-                    str+='</div>';
-                });
-                details.html(str);
+                    details.html(str);
+                }else{
+                    str+="<div class='empty align_center'><span>此手机号码还没有上传质保记录图！</span></div>";
+                    //$(".content").html(str);
+                    details.html(str);
+                }
+
                 //明细图片fadeIn
                 $(".detaPic").on("click", "li", function() {
                     $galleryImg.attr("src", this.getAttribute("realPath"));
@@ -171,10 +228,6 @@ function loadList(action, openid, keyword) {
 }
 
 
-//检索后加载列表
-$(function() {
-
-});
 
 //删除图片明细方法
 function galleryDel(obj) {
