def494cecd38efb1444731f71b22751fc4696ece
Chai <chai@fy.com>
2020-06-30 13:08:54 +0800

    验证手机号

--- static/js/utils/common.js
+++ static/js/utils/common.js
@@ -1,6 +1,6 @@
 // 验证手机号
 function isPhoneNo(phone) {
  if(!phone)return true;
- var pattern = /^1[3456789]\d{9}$/;
+ var pattern = /^1(?:3\d|4[4-9]|5[0-35-9]|6[67]|7[013-8]|8\d|9\d)\d{8}$/;
  return pattern.test(phone); 
 }
